8dea532ab40935731200b4f06d8f1b688fe48ccd
dev-wiqi <dev@wiqi.co>
2015-06-10 15:08:33 +0700

    Update Codeigniter V.3.0

---
+++ application/mod_core/home/controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+    if (!defined('BASEPATH'))
+        exit('No direct script access allowed');
+
+ class Home extends MX_Controller {
+    /*
+     * SumberNews Portal Application V.1.0 Copyright 2015
+     * Build Date : 6 Juni 2015
+     * Founder & Programmer : Wisnu Groho Aji 
+     * Website : http://wiqi.co
+     */
+     
+     function __construct() {
+         parent::__construct();
+         $this->
+     }
+     
+    
+ }
