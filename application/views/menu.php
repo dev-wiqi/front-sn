75c091c843282547572552584d49c685f5359f13
dev-wiqi <dev@wiqi.co>
2015-06-23 15:22:07 +0700

    tes jquery var

--- application/views/menu.php
+++ application/views/menu.php
@@ -4,10 +4,14 @@
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/
 libs/jquery/1.3.0/jquery.min.js"></script>
 <script type="text/javascript">
+var lastpost = '//localhost/front-sn/home/news_load/';
+var idhtml = '#news';
+</script>
+<script type="text/javascript">
 var auto_refresh = setInterval(
 function ()
 {
-$('#news').load('<?php echo base_url(); ?>home/news_load').effect("fade").fadeIn("slow");
+$(idhtml).load(lastpost).fadeIn("slow");
 }, 10000); // refresh every 10000 milliseconds
 </script>
 <div id="news"></div>
